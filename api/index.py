e70eee1320a4a912f058dce470ffc2a6cf47caf2
rbo-xyz <raffaele.boppart@icloud.com>
2024-12-27 18:59:01 +0100

    v0.2 - backend init

--- api/index.py
+++ api/index.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from pyproj import transform
 
-### Create FastAPI instance with custom docs and openapi url
-app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+app = FastAPI()
 
-@app.get("/api/py/helloFastApi")
-def hello_fast_api():
-    return {"message": "Hello from FastAPI"}
+@app.get("/api/")
+async def basisverzeichnis():
+    return {"status": "alles gut, es funktioniert Version 2"}
+
+@app.get("/api/wgs84lv95")
+async def wgs84lv95(easting:float, northing:float):
+    nEasting, nNorthing = transform(4326, 2056, easting, northing)
+    return {
+        "Koordinatensystem" : "LV95",
+        "easting" : nEasting,
+        "northing" : nNorthing
+    }
+
+## Abfrage: ?easting=47.514920&northing=7.657665
+# Soll Resultat: 2616499.69, 1262709.11
+
+@app.get("/api/lv95wgs84")
+async def lv95wgs84(easting:float, northing:float):
+    nEasting, nNorthing = transform(2056, 4326, easting, northing)
+    return {
+        "Koordinatensystem" : "WGS84",
+        "easting" : nEasting,
+        "northing" : nNorthing
+    }
+
+## Abfrage: ?easting=2616499.69&northing=1262709.11
+# Soll Resultat: 47.514920, 7.657665
--- next.config.js
+++ next.config.js
@@ -3,25 +3,11 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/py/:path*",
+        source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/:path*"
+            ? "http://127.0.0.1:8000/api/:path*"
             : "/api/",
-      },
-      {
-        source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/docs"
-            : "/api/py/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/openapi.json"
-            : "/api/py/openapi.json",
       },
     ];
   },
