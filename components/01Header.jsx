83142e84e093d625191ae595aa9eaab315c0aecb
rbo-xyz <raffaele.boppart@icloud.com>
2025-01-05 20:39:51 +0100

    v1.7 - bug fix vercel

--- components/01Header.jsx
+++ components/01Header.jsx
@@ -1,6 +1,6 @@
 // Vorlage von https://mui.com/material-ui/react-app-bar/
 
-import * as React from "react";
+import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
